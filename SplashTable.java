69d951df97ad3d63ebab0f12aaaf9436e34da3a7
asdas209 <asdas204@gmail.com>
2014-04-26 00:02:57 -0400

    Added main method to parse input values

--- SplashTable.java
+++ SplashTable.java
@@ -1,3 +1,25 @@
 public class SplashTable{
-
+	/*
+	*
+	*/
+	private int bucketSize;
+	private int reInsertions;
+	private int tableSize;
+	private int numMultipliers;
+	
+	public static void main(String args[]){
+		// Take in arguments
+		bucketSize = Integer.parseInt(args[0]);
+		reInsertions = Integer.parseInt(args[1]);
+		tableSize = Math.pow(Integer.parseInt(args[2]),2);
+		numMultipliers = Integer.parseInt(args[3]);
+		String inputFile = args[4];
+		if(args.size == 7){
+			String probeFile = args[6];
+			String resultFile = args[7];
+		}
+		String dumpFile = args[5];
+		String probeFile = args[6];
+		String resultFile = args[7];
+	}
 }
